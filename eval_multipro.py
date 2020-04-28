f8a68fafe56816efe19a315ddb78b12e17d65878
dbrisbin <dbrisbin@umich.edu>
2020-04-28 14:12:13 -0400

    some more bugs!

--- eval_multipro.py
+++ eval_multipro.py
@@ -12,7 +12,7 @@ from scipy.io import loadmat
 # Our libs
 from config import cfg
 from dataset import ValDataset
-from models import ModelBuilder, SegmentationModuleEval
+from models import ModelBuilder, SegmentationModule
 from utils import AverageMeter, colorEncode, accuracy, intersectionAndUnion, parse_devices, setup_logger
 from lib.nn import user_scattered_collate, async_copy_to
 from lib.utils import as_numpy
--- models/models.py
+++ models/models.py
@@ -31,35 +31,6 @@ class SegmentationModule(SegmentationModuleBase):
         # training
         if segSize is None:
             if self.deep_sup_scale is not None: # use deep supervision technique
-                (pred, pred_deepsup) = self.decoder(self.encoder(feed_dict[0]['img_data'].cuda(), return_feature_maps=True))
-            else:
-                pred = self.decoder(self.encoder(feed_dict[0]['img_data'].cuda(), return_feature_maps=True))
-
-            loss = self.crit(pred, feed_dict[0]['seg_label'].cuda())
-            if self.deep_sup_scale is not None:
-                loss_deepsup = self.crit(pred_deepsup, feed_dict[0]['seg_label'].cuda())
-                loss = loss + loss_deepsup * self.deep_sup_scale
-
-            acc = self.pixel_acc(pred, feed_dict[0]['seg_label'].cuda())
-            return loss, acc
-        # inference
-        else:
-            pred = self.decoder(self.encoder(feed_dict[0]['img_data'].cuda(), return_feature_maps=True), segSize=segSize)
-            return pred
-
-
-class SegmentationModuleEval(SegmentationModuleBase):
-    def __init__(self, net_enc, net_dec, crit, deep_sup_scale=None):
-        super(SegmentationModuleEval, self).__init__()
-        self.encoder = net_enc
-        self.decoder = net_dec
-        self.crit = crit
-        self.deep_sup_scale = deep_sup_scale
-
-    def forward(self, feed_dict, *, segSize=None):
-        # training
-        if segSize is None:
-            if self.deep_sup_scale is not None: # use deep supervision technique
                 (pred, pred_deepsup) = self.decoder(self.encoder(feed_dict['img_data'].cuda(), return_feature_maps=True))
             else:
                 pred = self.decoder(self.encoder(feed_dict['img_data'].cuda(), return_feature_maps=True))
@@ -75,7 +46,6 @@ class SegmentationModuleEval(SegmentationModuleBase):
         else:
             pred = self.decoder(self.encoder(feed_dict['img_data'].cuda(), return_feature_maps=True), segSize=segSize)
             return pred
-
 
 class ModelBuilder:
     # custom weights initialization
